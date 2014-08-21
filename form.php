f84cf70af6fbe6c8e3204db7002d8023fde360e9
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-20 21:40:32 -0400

    Update form.php

--- form.php
+++ form.php
@@ -67,7 +67,7 @@ function test_input($data) {
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-   Name: <input type="text" name="name" value="<?php echo $name;?>">
+   Name: <input type="text" name="name" value=<?php echo $name;?>>
    <span class="error">* <?php echo $nameErr;?></span>
    <br><br>
    E-mail: <input type="text" name="email" value="<?php echo $email;?>">
