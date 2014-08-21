6111d8e880da3ab9b7aa7686f795f6971d28983b
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-21 15:48:17 -0400

    Update form.php

--- form.php
+++ form.php
@@ -7,28 +7,10 @@
 	</head>
 	
 	<body> 
-	
-			<h2>Submit a New Bounty</h2>
-		<p><span class="error">* required field.</span></p>
-		
-			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-				Bounty Title: <input type="text" name="bountyTitle" value="<?php echo $bountyTitle;?>">
-				<span class="error">* <?php echo $bountyTitleErr;?></span>
-				<br><br>
-				Bounty Description: <textarea name="description" rows="5" cols="40"><?php echo $description;?></textarea>
-				<br><br>
-				Myriadcoin Address: <input type="text" name="myrAddress" value="<?php echo $myrAddress;?>">
-				<span class="error"><?php echo $myrAddressErr;?></span>
-				<br><br>
-				Username: <input type="text" name="userName" value="<?php echo $userName;?>">
-				<span class="error">* <?php echo $userNameErr;?></span>
-				<br><br>
-				<input type="submit" name="submit" value="Submit"> 
-			</form>
 
 <?php
 // define variables and set to empty values
-$bountyTitleErr = $descriptionErr $myrAddressErr = $userNameErr = "";
+$bountyTitleErr = $descriptionErr = $myrAddressErr = $userNameErr = "";
 $bountyTitle = $description = $myrAddress = $userName = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
@@ -79,7 +61,23 @@ function cleanInput($data)
 }
 ?> 
 
-
+			<h2>Submit a New Bounty</h2>
+		<p><span class="error">* required field.</span></p>
+		
+			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+				Bounty Title: <input type="text" name="bountyTitle" value="<?php echo $bountyTitle;?>">
+				<span class="error">* <?php echo $bountyTitleErr;?></span>
+				<br><br>
+				Bounty Description: <textarea name="description" rows="5" cols="40"><?php echo $description;?></textarea>
+				<br><br>
+				Myriadcoin Address: <input type="text" name="myrAddress" value="<?php echo $myrAddress;?>">
+				<span class="error"><?php echo $myrAddressErr;?></span>
+				<br><br>
+				Username: <input type="text" name="userName" value="<?php echo $userName;?>">
+				<span class="error">* <?php echo $userNameErr;?></span>
+				<br><br>
+				<input type="submit" name="submit" value="Submit"> 
+			</form>
 
 <?php
 echo "<h2>Your Input:</h2>";
