0b6164b92901339dca0b58f6c3e8ea9cdd60bc8d
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-21 14:10:48 -0400

    Update form.php

--- form.php
+++ form.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	if (empty($_POST["bountyTitle"])) 
 	{
-		$bountyTitleErr = A title is required";
+		$bountyTitleErr = "A title is required";
     } 
     else 
     {
