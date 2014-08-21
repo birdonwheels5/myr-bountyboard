e7bfb52148c288a2584893d3274f982e21e36e6b
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-21 13:49:02 -0400

    Update form.php

--- form.php
+++ form.php
@@ -62,18 +62,18 @@ function cleanInput($data) {
 		<h2>Submit a New Bounty</h2>
 		<p><span class="error">* required field.</span></p>
 			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-			Bounty Title: <input type="text" title="bountyTitle" value="<?php echo $bountyTitle;?>">
+			Bounty Title: <input type="text" name="bountyTitle" value="<?php echo $bountyTitle;?>">
 			<span class="error">* <?php echo $bountyTitleErr;?></span>
 			<br><br>
-			Myriadcoin Address: <input type="text" title="myrAddress" value="<?php echo $myrAddress;?>">
+			Myriadcoin Address: <input type="text" name="myrAddress" value="<?php echo $myrAddress;?>">
 			<span class="error"><?php echo $myrAddressErr;?></span>
 			<br><br>
-			Bounty Description: <textarea title="description" rows="5" cols="40"><?php echo $description;?></textarea>
+			Bounty Description: <textarea name="description" rows="5" cols="40"><?php echo $description;?></textarea>
 			<br><br>
-			userName: <input type="text" title="userName" value="<?php echo $userName;?>">
+			userName: <input type="text" name="userName" value="<?php echo $userName;?>">
 			<span class="error">* <?php echo $userNameErr;?></span>
 			<br><br>
-			<input type="submit" title="submit" value="Submit"> 
+			<input type="submit" name="submit" value="Submit"> 
 	</form>
 
 <?php
