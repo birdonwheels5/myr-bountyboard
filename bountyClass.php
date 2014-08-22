afebace1bf527c49fbcc952eaf6ca9b6ece1e080
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-22 02:09:46 -0400

    Create bountyClass.php

---
+++ bountyClass.php
@@ -0,0 +1,17 @@
+<?php
+class bounty
+{
+  $title = "";
+  
+  
+  
+  function __construct($par1Title, $par1Description, $par1MyrAddress, $par1UserName, $par1IsActive)
+  {
+    
+  }
+  
+  
+}
+
+
+?>
