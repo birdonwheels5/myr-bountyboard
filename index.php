95dd2fbc90f6c6eb5f221708f4963f14c5272782
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-21 21:09:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,31 +21,34 @@
 		$index = 0;
 		
 		// Check the current line for "-" which separates bounties
-		if (strcasecmp(strpbrk($line, "-"),"-") == 0)
+		if (strcmp(strpbrk($line, "-"),"-") == 0)
 		{
 		}
 		// Fetch bounty information line-by-line, construct the bounty, then add 1 to bounty count
 		else
 		{
-			if (strpbrk($line,"title: ") == "title: ")
+			if (stristr($line,"title: ") == "title: ")
 			{
-				str_ireplace("title: ", "", $line);
+				strpbrk($line,"title: ");
 				$title = $line;
 			}
 			
 			if (strpbrk($line,"desc: ") == "desc: ")
 			{
-				str_ireplace("desc: ", "", $description);
+				str_ireplace("desc: ", "", $line);
+				$description = $line;
 			}
 		
 			if (strpbrk($line,"addr: ") == "addr: ")
 			{
-				str_ireplace("addr: ", "", $myrAddress);
+				str_ireplace("addr: ", "", $line);
+				$myrAddress = $line;
 			}
 		
 			if (strpbrk($line,"user: ") == "user: ")
 			{
-				str_ireplace("user: ", "", $userName);
+				str_ireplace("user: ", "", $line);
+				$userName = $line;
 			}
 		
 			// bounty.create($title, $description, $myrAddress, $userName)
