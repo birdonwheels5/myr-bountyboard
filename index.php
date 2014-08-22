8d100f39a07ba27a5f548523fe852375ef992bb9
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-22 17:13:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 
 	<head>
-	<?php include ("classBounty.php");
-	      include ("bountyReader.php");
+	<?php include "classBounty.php";
+	      include "bountyReader.php";
 	      error_reporting(E_ALL);
 	?>
 	</head>
