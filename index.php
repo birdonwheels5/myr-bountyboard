562bd01f20558c0b118d6ec166572b4eb442ad86
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-21 21:55:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 		// Fetch bounty information line-by-line, construct the bounty, then add 1 to bounty count
 		if (strcmp(stristr($line,"title: "), $line) == 0)
 		{
-			str_ireplace("title: ", "", $line);
+			$line = str_ireplace("title: ", "", $line);
 			$title = $line;
 		}
 		
