c939027205042ebb540b04beaa596ec7ab3696cd
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-22 19:02:18 -0400

    Update bountyReader.php

--- bountyReader.php
+++ bountyReader.php
@@ -77,11 +77,10 @@ function readBounties($fileName)
 			echo "---------";
 			echo "<br>";
 			//$array[index] = new bounty($title, $description, $myrAddress, $userName, $active);
-			$index++;
 		}
 	}
 	fclose($file);
-	return index;
+	//return $bounties;
 }
 
 function countBounties($fileName)
@@ -99,6 +98,91 @@ function countBounties($fileName)
 	}
 	fclose($file);
 	return index;
+}
+
+// ---------------------------
+
+class bounty
+{
+  var $title = "";
+  var $description = "";
+  var $myrAddress = "";
+  var $userName = "";
+  var $active = "";
+  
+  
+  
+  function __construct($par1Title, $par1Description, $par1MyrAddress, $par1UserName, $par1Active)
+  {
+    $this->title = $par1Title;
+    $this->description = $par1Description;
+    $this->myrAddress = $par1MyrAddress;
+    $this->userName = $par1UserName;
+    $this->active = $par1Active;
+    
+
+    /*setTitle($par1Title);
+    setDescription($par1Description);
+    setMyrAddress($par1MyrAddress);
+    setUserName($par1UserName);
+    setActive($par1Active);2*/
+  }
+  
+  
+  
+  /*function setTitle($newTitle)
+  {
+    $this->$title = $newTitle;
+  }
+  
+  function setDescription($newDescription)
+  {
+    $this->$description = $newDescription;
+  }
+  
+  function setMyrAddress($newMyrAddress)
+  {
+    $this->$myrAddress = $newMyrAddress;
+  }
+  
+  function setUserName($newUserName)
+  {
+    $this->$userName = $newUserName;
+  }
+  
+  function setActive($newActive)
+  {
+    $this->$active = $newActive;
+  }*/
+  
+  
+  
+  function getTitle()
+  {
+    return $this->title;
+  }
+  
+  function getDescription()
+  {
+    return $this->description;
+  }
+  
+  function getMyrAddress()
+  {
+    return $this->myrAddress;
+  }
+  
+  function getUserName()
+  {
+    return $this->userName;
+  }
+  
+  function getActive()
+  {
+    return $this->active;
+  }
+  
+  
 }
 
 
