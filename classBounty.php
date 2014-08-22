470cf2e570a3f9a4e9fc5edffda1e1c274d0c1af
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-22 18:21:48 -0400

    Update classBounty.php

--- classBounty.php
+++ classBounty.php
@@ -11,16 +11,23 @@ class bounty
   
   function __construct($par1Title, $par1Description, $par1MyrAddress, $par1UserName, $par1Active)
   {
-    setTitle($par1Title);
+    $this->title = $par1Title;
+    $this->description = $par1Description;
+    $this->myrAddress = $par1MyrAddress;
+    $this->userName = $par1UserName;
+    $this->active = $par1Active;
+    
+
+    /*setTitle($par1Title);
     setDescription($par1Description);
     setMyrAddress($par1MyrAddress);
     setUserName($par1UserName);
-    setActive($par1Active);
+    setActive($par1Active);2*/
   }
   
   
   
-  function setTitle($newTitle)
+  /*function setTitle($newTitle)
   {
     $this->$title = $newTitle;
   }
@@ -43,33 +50,33 @@ class bounty
   function setActive($newActive)
   {
     $this->$active = $newActive;
-  }
+  }*/
   
   
   
   function getTitle()
   {
-    return $this->$title;
+    return $this->title;
   }
   
   function getDescription()
   {
-    return $this->$description;
+    return $this->description;
   }
   
   function getMyrAddress()
   {
-    return $this->$myrAddress;
+    return $this->myrAddress;
   }
   
   function getUserName()
   {
-    return $this->$userName;
+    return $this->userName;
   }
   
   function getActive()
   {
-    return $this->$active;
+    return $this->active;
   }
   
   
