bb5a4a7e1557d6bcfa16b5159290eb841486b411
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-22 19:59:02 -0400

    Update bountyReader.php

--- bountyReader.php
+++ bountyReader.php
@@ -1,5 +1,7 @@
 <?php
 
+include "Bounty.php";
+
 function readBounties($fileName)
 {
 	$debugMode = true;
@@ -80,8 +82,6 @@ function readBounties($fileName)
 				echo "---------";
 				echo "<br>";	
 			}
-			
-			//$array[index] = new bounty($title, $description, $myrAddress, $userName, $active);
 		}
 	}
 	fclose($file);
@@ -106,92 +106,4 @@ function countBounties($fileName)
 	fclose($file);
 	return index;
 }
-?>
-
-<?php
-
-class Bounty
-{
-  private $title = "";
-  private $description = "";
-  private $myrAddress = "";
-  private $userName = "";
-  private $active = "";
-  
-  
-  
-  function __construct($par1Title, $par1Description, $par1MyrAddress, $par1UserName, $par1Active)
-  {
-    $this->title = $par1Title;
-    $this->description = $par1Description;
-    $this->myrAddress = $par1MyrAddress;
-    $this->userName = $par1UserName;
-    $this->active = $par1Active;
-    
-
-    /*setTitle($par1Title);
-    setDescription($par1Description);
-    setMyrAddress($par1MyrAddress);
-    setUserName($par1UserName);
-    setActive($par1Active);*/
-  }
-  
-  
-  
-  /*function setTitle($newTitle)
-  {
-    $this->$title = $newTitle;
-  }
-  
-  function setDescription($newDescription)
-  {
-    $this->$description = $newDescription;
-  }
-  
-  function setMyrAddress($newMyrAddress)
-  {
-    $this->$myrAddress = $newMyrAddress;
-  }
-  
-  function setUserName($newUserName)
-  {
-    $this->$userName = $newUserName;
-  }
-  
-  function setActive($newActive)
-  {
-    $this->$active = $newActive;
-  }*/
-  
-  
-  
-  function getTitle()
-  {
-    return $this->title;
-  }
-  
-  function getDescription()
-  {
-    return $this->description;
-  }
-  
-  function getMyrAddress()
-  {
-    return $this->myrAddress;
-  }
-  
-  function getUserName()
-  {
-    return $this->userName;
-  }
-  
-  function getActive()
-  {
-    return $this->active;
-  }
-  
-  
-}
-
-
 ?>
