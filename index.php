f36776508ea22a6336fe9383623e595c4d46384e
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-22 15:20:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
 
 	<head>
+	<?php include ("class_bounty.php");
+	      include ("bountyReader.php");
+	?>
 	</head>
 
 	<body>
@@ -18,75 +21,9 @@
 	$index = 0;
 	
 	$fileName = "bounties.dat";
-	$handle = fopen($fileName, "r") or print ("Error loading bounties!");
-    	while (($line = fgets($handle)) !== false) 
-    	{
-		
-		// Fetch bounty information line-by-line, construct the bounty, then add 1 to bounty count
-		
-		
-		// Check for Enter keystrokes and concatenate new lines to the Description.
-		if ((strcmp(stristr($line,"title: "), $line) != 0) or (strcmp(stristr($line,"desc: "), $line) != 0) or 
-		    (strcmp(stristr($line,"addr: "), $line) != 0) or (strcmp(stristr($line,"user: "), $line) != 0) or 
-		    (strcmp(stristr($line,"active: "), $line) != 0) or
-		    (strcmp(stristr($line,"-"), $line) != 0))
-		{
-			if (strcmp(stristr($line,$description), $line) != 0 and 
-			   (strcmp(stristr($line,"addr: "), $line) != 0) and 
-			   (strcmp(stristr($line,"user: "), $line) != 0) and 
-			   (strcmp(stristr($line,"active: "), $line) != 0) and 
-			   (strcmp(stristr($line,"-"), $line) != 0))
-			{
-				$description = $description . "\n" . "<br>" . "\n" . $line;
-			}
-		}
-		
-		if (strcmp(stristr($line,"title: "), $line) == 0)
-		{
-			$title = str_ireplace("title: ", "", $line);
-		}
-		
-		if (strcmp(stristr($line,"desc: "), $line) == 0)
-		{
-			$description = str_ireplace("desc: ", "", $line);
-		}
 	
-		if (strcmp(stristr($line,"addr: "), $line) == 0)
-		{
-			$myrAddress = str_ireplace("addr: ", "", $line);
-		}
+	readBounties($fileName);
 	
-		if (strcmp(stristr($line,"user: "), $line) == 0)
-		{
-			$userName = str_ireplace("user: ", "", $line);
-		}
-		
-		if (strcmp(stristr($line,"active: "), $line) == 0)
-		{
-			$active = str_ireplace("active: ", "", $line);
-		}
-
-		// Check the current line for "-" which separates bounties, and create bounties
-		if (strcmp(stristr($line, "-"), $line) == 0)
-		{
-			echo $title;
-    			echo "<br>";
-			echo $description;
-			echo "<br>";
-			echo $myrAddress;
-			echo "<br>";
-			echo $userName;
-			echo "<br>";
-			echo $active;
-			echo "<br>";
-			echo "---------";
-			echo "<br>";
-			//$array[index] = new bounty($title, $description, $myrAddress, $userName, $active);
-			$index++;
-		}
-	}
-    	
-	fclose($file);
 	?>
 	
 	
