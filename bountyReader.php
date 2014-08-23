198bb3ec2763721d0bc00770735d5d87ab08b35a
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-22 22:50:49 -0400

    Update bountyReader.php

--- bountyReader.php
+++ bountyReader.php
@@ -28,7 +28,7 @@ function readBounties($fileName)
 		if ((strcmp(stristr($line,"title: "), $line) != 0) or (strcmp(stristr($line,"desc: "), $line) != 0) or 
 		    (strcmp(stristr($line,"addr: "), $line) != 0) or (strcmp(stristr($line,"user: "), $line) != 0) or 
 		    (strcmp(stristr($line,"active: "), $line) != 0) or
-		    (strcmp(stristr($line,"-"), $line) != 0))
+		    (strcmp(stristr($line, $separator), $line) != 0))
 		{
 			if (strcmp(stristr($line,$description), $line) != 0 and 
 			   (strcmp(stristr($line,"addr: "), $line) != 0) and 
