ddf453f3b63f053ee837cbc4c809b18f497f9756
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-23 13:52:50 -0400

    Update deleteForm.php

--- deleteForm.php
+++ deleteForm.php
@@ -21,51 +21,16 @@ $WAITING = -1;
 $FAILURE = 1;
 $SUCCESS = 0;
 
-$bountySubmitted = WAITING;
+$fileName = "bounties.dat";
+$bountyDeleted = WAITING;
+$redirectURL = "https://birdonwheels5.no-ip.org/myr-bountyboard/";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
-	if (empty($_POST["title"])) 
-	{
-		$titleErr = "A title is required";
-	} 
-    	else 
-    	{
-		$title = cleanInput($_POST["title"]);
-    	}
+	$_POST["title"] = bounties[x]
     	
 //TODO Implement title search and throw error if title already exists
-	if (strcmp($title))
-	{
-		
-	}
 
-    	if (empty($_POST["description"])) 
-	{
-      		$descriptionErr = "A description of your bounty is required";
-    	} 
-	else 
-	{
-      		$description = cleanInput($_POST["description"]);
-    	}
-    
-	if (empty($_POST["myrAddress"])) 
-	{
-		$myrAddressErr = "A Myriadcoin address is required";
-    	} 
-	else 
-	{
-      		$myrAddress = cleanInput($_POST["myrAddress"]);
-    	}
-   
-    	if (empty($_POST["userName"])) 
-	{
-      		$userNameErr = "Your username is required";
-    	} 
-	else 
-	{
-      		$userName = cleanInput($_POST["userName"]);
-    	}
     	
 $fileName = "bounties.dat";
 $separator = "&-$";
@@ -74,64 +39,52 @@ $active = "true";
 
 if((strcmp($title, $empty) == 0) or (strcmp($description, $empty) == 0) or (strcmp($myrAddress, $empty) == 0) or (strcmp($userName, $empty) == 0))
 {
-	$bountySubmitted = FAILURE;
+	$bountyDeleted = FAILURE;
 }
 else
 {
-	$bountySubmitted = SUCCESS;
+	$bountyDeleted = SUCCESS;
 	file_put_contents($fileName, "title: " . $title . "\n" . "desc: " . $description  . "\n" . "addr: " . $myrAddress . "\n" . "user: " . $userName . "\n" . "active: " . $active . "\n" . $separator . "\n", FILE_APPEND);
 }
-}
-
-function cleanInput($data) 
-{
-    $data = trim($data);
-    $data = ltrim($data,"^M");
-    $data = htmlspecialchars($data);
-    return $data;
 }
 ?> 
 
 			<h2>Submit a New Bounty</h2>
-		<p><span class="error">* required field.</span></p>
+		<p><span class="error">Review bounty before deleting it!</span></p>
 		
 			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
 				Bounty Title:<br>
 				<input type="text" name="title" value="<?php echo $title;?>">
-				<span class="error">* <?php echo $titleErr;?></span>
-				
+
 				<br><br>
 				Bounty Description:<br>
 				<textarea name="description" rows="5" cols="40"><?php echo $description;?></textarea>
-				<span class="error">* <?php echo $descriptionErr;?></span>
 
 				<br><br>
 				
 				Myriadcoin Address: <br>
 				<input type="text" name="myrAddress" value="<?php echo $myrAddress;?>">
-				<span class="error">* <?php echo $myrAddressErr;?></span>
 				<br><br>
 				
 				Username:<br>
 				<input type="text" name="userName" value="<?php echo $userName;?>">
-				<span class="error">* <?php echo $userNameErr;?></span>
 				<br><br>
 				
 				<input type="submit" name="submit" value="Submit"> 
 			</form>
 
 <?php
-if ($bountySubmitted == FAILURE)
+if ($bountyDeleted == FAILURE)
 {
-	print "Bounty submission failed!";
+	print "Bounty deletion failed! (Could not find bounty)";
 	print $description;
 }
-else if ($bountySubmitted == SUCCESS)
+else if ($bountyDeleted == SUCCESS)
 {
-	print "Bounty submission successful!";
+	print "Bounty deleted!";
 	print "<br>";
 	print "Redirecting to bounty page...";
-	header("Refresh: 3, URL = https://birdonwheels5.no-ip.org/myr-bountyboard/");
+	header("Refresh: 3, URL = " . $redirectURL);
 	exit;
 }
 ?>
