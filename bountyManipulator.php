505ab3766b42e79a6798299e39f66b7a67b8ebd6
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-23 17:13:09 -0400

    Update bountyManipulator.php

--- bountyManipulator.php
+++ bountyManipulator.php
@@ -184,7 +184,7 @@ function searchBounty($fileName, $title)
 	
 	$searchResult = $FAILURE;
 	
-	for($i = bountyCount; (strcmp(stristr($bounties[i]->getTitle(), $titleResult), $bounties[i]->getTitle()) == 0); $i++)
+	for($i = $bountyCount; (strcmp(stristr($bounties[i]->getTitle(), $titleResult), $bounties[i]->getTitle()) == 0); $i++)
 	{
 		$searchResult = i;
 	}
