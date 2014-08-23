1d3d32da12b291d80466241fe73699e1124e7d0e
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-22 22:51:56 -0400

    Update bountyReader.php

--- bountyReader.php
+++ bountyReader.php
@@ -36,7 +36,7 @@ function readBounties($fileName)
 			   (strcmp(stristr($line,"active: "), $line) != 0) and 
 			   (strcmp(stristr($line, $separator), $line) != 0))
 			{
-				$description = $description . "\n" . "<br>" . "\n" . $line;
+				$description = $description . "<br>" . $line;
 			}
 		}
 		
