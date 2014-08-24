f7e84fa9cc9e1ffb1e3c4979fa454f2e278c9ee4
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-24 12:53:50 -0400

    Update deleteForm.php

--- deleteForm.php
+++ deleteForm.php
@@ -56,7 +56,7 @@ if ($bountyNumber > 0)
 	if ($confirmFlag == true);
 	{
 		$bountyDeleted = $SUCCESS;
-		removeBounty($fileName, "title: " . cleanInput($_POST["title"]));	
+		removeBounty($fileName, "title: " . $title);	
 	}
 	
 	if ($confirmFlag == false)
