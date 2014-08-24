0c5bc321bc53a87df89df909ce392bc311c43536
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-24 19:08:20 -0400

    Update bountyManipulator.php

--- bountyManipulator.php
+++ bountyManipulator.php
@@ -359,7 +359,7 @@ function searchBounty($fileName, $title)
  		
  			if ($i == $lineNumber)
  			{ // and
- 				$content .= $lines[$i] . chr(10) . $stringToAdd . chr(10); // get
+ 				$content .= $lines[$i] . $stringToAdd . chr(10); // get
  			} 
  			else 
  			{ // the
