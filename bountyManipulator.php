cb8c8755c882961ced238163a41d0f28ba48a5f4
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-24 13:24:19 -0400

    Update bountyManipulator.php

--- bountyManipulator.php
+++ bountyManipulator.php
@@ -291,7 +291,7 @@ function searchBounty($fileName, $title)
  		// Makes sure that only the first occurance is overwritten	
  		$replaceCount = 0;
  		
- 		for($i = 0; $i < ($lineNumber - 1); $i++)
+ 		for($i = 1; $i < ($lineNumber - 1); $i++)
  		{
  			$content .= $lines[$i]; // content.
  		}
