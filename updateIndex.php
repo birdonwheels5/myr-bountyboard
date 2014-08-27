9dbd4f1dee7617aa496314d30408f35355039ff7
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-27 01:27:31 -0400

    Update updateIndex.php

--- updateIndex.php
+++ updateIndex.php
@@ -44,11 +44,11 @@
 					<center><b>Total Bounties</b>: " . $bountyCount . "</center><br/>
 					A few things to note before getting started...<br/>
 					<ul>
-						<li>The green bounties indicate bounties that are up for grabs, while the gray bounties are already claimed.
-						<li>Please contact a moderator on our <a href=\"http://www.reddit.com/r/myriadcoin\">subreddit</a> if you are interested in taking up a bounty, and then again once you have completed it to claim your reward. They can also be found on <a href=\"http://webchat.freenode.net/?nick=myriad%7C...&channels=%23%23myriadcoin&uio=OT10cnVlJjExPTEyMyYxMj10cnVl38\"> our IRC channel</a>. You may wish to keep them in the loop with your progress.
-						<li>Because of long loading times when there are numerous bounties, the list is compiled hourly. So any new (or edited) bounties will appear then.
-						<li>Whenever a new bounty is added or an old one is updated, that bounty will drop to the bottom of the page. Be sure to check the bottom!
-						<li>The block explorer may report a different amount than the amount raised for the bounty, but be assured that the amount displayed on this page will be paid out upon the bounty's completion.
+						<li>The green bounties indicate bounties that are up for grabs, while the gray bounties are already claimed.<br/>
+						<li>Please contact a moderator on our <a href=\"http://www.reddit.com/r/myriadcoin\">subreddit</a> if you are interested in taking up a bounty, and then again once you have completed it to claim your reward. They can also be found on <a href=\"http://webchat.freenode.net/?nick=myriad%7C...&channels=%23%23myriadcoin&uio=OT10cnVlJjExPTEyMyYxMj10cnVl38\"> our IRC channel</a>. You may wish to keep them in the loop with your progress.<br/>
+						<li>Because of long loading times when there are numerous bounties, the list is compiled hourly. So any new (or edited) bounties will appear then.<br/>
+						<li>Whenever a new bounty is added or an old one is updated, that bounty will drop to the bottom of the page. Be sure to check the bottom!<br/>
+						<li>The block explorer may report a different amount than the amount raised for the bounty, but be assured that the amount displayed on this page will be paid out upon the bounty's completion.<br/>
 					</ul>
 					<center>Good luck, brave Bounty Hunters!</center></p>
 				</div>
@@ -108,7 +108,7 @@
 	}
 	
 	$content .= "</article><div class=\"paddingBottom\"></div>
-		     <footer>Want to show some love? Donate some MYR: MNYERWCHqrH1EkGNpF4T8o8dGB391A5jmm</footer></div>";
+		     <footer>Bounty page by birdonwheels5. Want to show some love? Donate some MYR: MNYERWCHqrH1EkGNpF4T8o8dGB391A5jmm</footer></div>";
 
 	$content .= "</body></html>";
 
