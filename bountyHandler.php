77fed283d54469cc19cdadce684838151fbe3986
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-27 12:45:47 -0400

    Update bountyHandler.php

--- bountyHandler.php
+++ bountyHandler.php
@@ -193,6 +193,127 @@ function displayBountyInfo($fileName)
 
 // -----------------------------------------------------------------------------------------
 
+// Create the index.php file that will be served to your visitors.
+function displayBounties()
+{
+	include "bountyHandler.php";
+	
+	$fileName = "bounties.dat";
+	
+	$content = "";
+	
+	$bounties = array();
+	$bounties = readBounties($fileName);
+	
+	$bountyCount = countBounties($fileName);
+	
+	$content .= "<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset=\"ISO-8859-1\">
+		<title>MYR Bounty Board</title>
+		<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\" title=\"Default Styles\" media=\"screen\"/>
+		<link rel=\"stylesheet\" type=\"text/css\" href=\"http://fonts.googleapis.com/css?family=Open+Sans\" title=\"Font Styles\"/>
+	</head>
+	
+	<body link=\"#E2E2E2\" vlink=\"#000000\">
+		<center><div class=\"container\">
+	
+		
+			<header style=\"background-color:transparent;\">
+				<table>
+					<tr>
+						<td><img src=\"http://i.imgur.com/7JOcOBP.png\" style=\"width:120%;\"></img></td>
+						<td><h1>&nbsp;	&nbsp;	Community Bounty Board</h1></td>
+					</tr>
+				</table>
+			</header>
+			
+			<article>
+
+				<hr/>
+			
+				<div class=\"welcome\">
+					<center><p>Welcome to the <b>Myriadcoin Community Bounty Board</b>!</center><br/>
+					<center><b>Total Bounties</b>: " . $bountyCount . "</center><br/>
+					A few things to note before getting started...<br/>
+					<ul>
+						<li>The green bounties indicate bounties that are up for grabs, while the gray bounties are already claimed.<br/>
+						<li>Please contact a moderator on our <a href=\"http://www.reddit.com/r/myriadcoin\">subreddit</a> if you are interested in taking up a bounty, and then again once you have completed it to claim your reward. They can also be found on <a href=\"http://webchat.freenode.net/?nick=myriad%7C...&channels=%23%23myriadcoin&uio=OT10cnVlJjExPTEyMyYxMj10cnVl38\"> our IRC channel</a>. You may wish to keep them in the loop with your progress.<br/>
+						<li>Because of long loading times when there are numerous bounties, the list is compiled hourly. So any new (or edited) bounties will appear then.<br/>
+						<li>Whenever a new bounty is added or an old one is updated, that bounty will drop to the bottom of the page. Be sure to check the bottom!<br/>
+						<li>The block explorer may report a different amount than the amount raised for the bounty, but be assured that the amount displayed on this page will be paid out upon the bounty's completion.<br/>
+					</ul>
+					<center>Good luck, brave Bounty Hunters!</center></p>
+				</div>
+				
+				<center><div class=\"divider\">
+					<div class=\"box\">
+						<p>Title</p>
+					</div>
+					
+					<div class=\"descBox\">
+						<p>Description</p>
+					</div>
+					
+					<div class=\"box\">
+						<center><p>Total MYR Raised</p></center>
+					</div>
+					
+					<div class=\"addressBox\" style=\"font-size:initial;\">
+						<p>MYR Address for Donations<p>
+					</div>
+					
+					<div class=\"numberBox\">
+						<center><p># of Donations<p></center>
+					</div>
+					
+					<div class=\"box\">
+						<p>Curator</p>
+					</div>
+				</div>";
+				
+	for($i = 1; $i <= $bountyCount; $i++)
+	{
+		if ((strcmp(stristr($bounties[$i]->getActive(), "true"), $bounties[$i]->getActive()) == 0))
+		{
+			$content .= "<div class=\"activeBounty\"><div class=\"box\" style=\"text-align:left;padding-left:10px;\"><p>" . $bounties[$i]->getTitle() . "</p>";
+			$content .= "</div><div class=\"descBox\" style=\"text-align:left;\"><p>" . $bounties[$i]->getDescription() . "</p>";
+				$addressTotal = getAddressTotal($bounties[$i]->getMyrAddress());
+			$content .= "</div><div class=\"box\"><center><p>" . $addressTotal . "</p></center>";
+			$content .= "</div><div class=\"addressBox\"><p><a href=\"http://birdonwheels5.no-ip.org:3000/address/" . $bounties[$i]->getMyrAddress() . "\">" . $bounties[$i]->getMyrAddress() . "</a></p>";
+				$donationCount = getDonationCount($bounties[$i]->getMyrAddress());
+			$content .= "</div><div class=\"numberBox\"><center><p>" . $donationCount . "</p></center>";
+			$content .= "</div><div class=\"box\"><p>" . $bounties[$i]->getUserName() . "</p>";
+			$content .= "</div></div>";
+		}
+		else
+		{
+			$content .= "<div class=\"inActiveBounty\"><div class=\"box\" style=\"text-align:left;padding-left:10px;\"><p>" . $bounties[$i]->getTitle() . "</p>";
+			$content .= "</div><div class=\"descBox\" style=\"text-align:left;\"><p>" . $bounties[$i]->getDescription() . "</p>";
+				$addressTotal = getAddressTotal($bounties[$i]->getMyrAddress());
+			$content .= "</div><div class=\"box\"><center><p>" . $addressTotal . "</p></center>";
+			$content .= "</div><div class=\"addressBox\"><p><a href=\"http://birdonwheels5.no-ip.org:3000/address/" . $bounties[$i]->getMyrAddress() . "\">" . $bounties[$i]->getMyrAddress() . "</a></p>";
+				$donationCount = getDonationCount($bounties[$i]->getMyrAddress());
+			$content .= "</div><div class=\"numberBox\"><center><p>" . $donationCount . "</p></center>";
+			$content .= "</div><div class=\"box\"><p>" . $bounties[$i]->getUserName() . "</p>";
+			$content .= "</div></div>";
+		}
+	}
+	
+	$content .= "</article><div class=\"paddingBottom\"></div>
+		     <footer>Bounty page by birdonwheels5. Want to show some love? Donate some MYR: MNYERWCHqrH1EkGNpF4T8o8dGB391A5jmm</footer></div>";
+
+	$content .= "</body></html>";
+
+	$fi = fopen("index.php", "w");
+ 	fwrite($fi, $content);
+ 	fclose($fi);
+}
+
+// -----------------------------------------------------------------------------------------
+
 // Returns a string with the total amount of MYR every received by the supplied address.
 // Not safe to cast to an int because of the "MYR" at the end of the string.
 function getAddressTotal($myrAddress)
